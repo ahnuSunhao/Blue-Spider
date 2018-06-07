ca56b6c42834969b80ec5b18182bd9132b4b185b
ahnuYangYufei <2430156274@qq.com>
2018-06-07 19:09:44 +0800

    interface1.0

---
+++ BlueSpider1.0/Blue Spider/ElectronicName.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Blue_Spider
+{
+    partial class ElectronicName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_total_com = new System.Windows.Forms.Label();
+            this.label_online_com = new System.Windows.Forms.Label();
+            this.label_sign_in = new System.Windows.Forms.Label();
+            this.list_electronic_name = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 465);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "签到人数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 465);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "在线电脑数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 465);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "总电脑数：";
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Tan;
+            this.btn_close.Location = new System.Drawing.Point(558, 217);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(93, 32);
+            this.btn_close.TabIndex = 14;
+            this.btn_close.Text = "关闭";
+            this.btn_close.UseVisualStyleBackColor = false;
+            // 
+            // btn_export
+            // 
+            this.btn_export.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_export.Location = new System.Drawing.Point(558, 155);
+            this.btn_export.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(93, 32);
+            this.btn_export.TabIndex = 13;
+            this.btn_export.Text = "导出";
+            this.btn_export.UseVisualStyleBackColor = false;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.BackColor = System.Drawing.Color.Silver;
+            this.btn_stop.Location = new System.Drawing.Point(558, 93);
+            this.btn_stop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(93, 32);
+            this.btn_stop.TabIndex = 11;
+            this.btn_stop.Text = "停止";
+            this.btn_stop.UseVisualStyleBackColor = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_start.Location = new System.Drawing.Point(558, 35);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(93, 32);
+            this.btn_start.TabIndex = 10;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(15, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "学生列表：";
+            // 
+            // label_total_com
+            // 
+            this.label_total_com.AutoSize = true;
+            this.label_total_com.Location = new System.Drawing.Point(86, 465);
+            this.label_total_com.Name = "label_total_com";
+            this.label_total_com.Size = new System.Drawing.Size(23, 15);
+            this.label_total_com.TabIndex = 22;
+            this.label_total_com.Text = "**";
+            // 
+            // label_online_com
+            // 
+            this.label_online_com.AutoSize = true;
+            this.label_online_com.Location = new System.Drawing.Point(243, 465);
+            this.label_online_com.Name = "label_online_com";
+            this.label_online_com.Size = new System.Drawing.Size(23, 15);
+            this.label_online_com.TabIndex = 23;
+            this.label_online_com.Text = "**";
+            // 
+            // label_sign_in
+            // 
+            this.label_sign_in.AutoSize = true;
+            this.label_sign_in.Location = new System.Drawing.Point(361, 465);
+            this.label_sign_in.Name = "label_sign_in";
+            this.label_sign_in.Size = new System.Drawing.Size(23, 15);
+            this.label_sign_in.TabIndex = 24;
+            this.label_sign_in.Text = "**";
+            // 
+            // list_electronic_name
+            // 
+            this.list_electronic_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.list_electronic_name.FormattingEnabled = true;
+            this.list_electronic_name.ItemHeight = 15;
+            this.list_electronic_name.Location = new System.Drawing.Point(19, 38);
+            this.list_electronic_name.Name = "list_electronic_name";
+            this.list_electronic_name.Size = new System.Drawing.Size(523, 409);
+            this.list_electronic_name.TabIndex = 25;
+            // 
+            // ElectronicName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(683, 518);
+            this.Controls.Add(this.list_electronic_name);
+            this.Controls.Add(this.label_sign_in);
+            this.Controls.Add(this.label_online_com);
+            this.Controls.Add(this.label_total_com);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ElectronicName";
+            this.Text = "电子点名";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_total_com;
+        private System.Windows.Forms.Label label_online_com;
+        private System.Windows.Forms.Label label_sign_in;
+        private System.Windows.Forms.ListBox list_electronic_name;
+    }
+}
