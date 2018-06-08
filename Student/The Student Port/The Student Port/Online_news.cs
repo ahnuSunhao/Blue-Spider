5d96a54285972da05ca2541b797f78a9a3578c29
ahnuWuwenhui <1204744256@qq.com>
2018-06-08 14:41:34 +0800

    the student port1.0

---
+++ Student/The Student Port/The Student Port/Online_news.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace The_Student_Port
+{
+    public partial class Online_news : Form
+    {
+        public Online_news()
+        {
+            InitializeComponent();
+        }
+    }
+}
