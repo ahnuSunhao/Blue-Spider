4319bd579ccc67df60fe2a42b5a2b152ed0c75a4
ahnuSunhao <1187174926@qq.com>
2018-06-11 23:54:50 +0800

    based function's implementation

---
+++ BlueSpider1.0/Blue Spider/tools/Until.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Blue_Spider.tools
+{
+    public class Until
+    {
+        public static void Log(params System.Object[] message)
+        {
+            string str = "";
+            if (message == null || message.Length == 0)
+            {
+                str = "null";
+            }
+            else
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    str += message[i];
+                }
+            }
+            Debug.WriteLine(str);
+        }
+    }
+}
