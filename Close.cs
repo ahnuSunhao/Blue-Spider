258f01a932a5a5c207fc27f96179b5f3e618e02f
ahnuTcy <1730452119@qq.com>
2018-06-12 12:00:50 +0800

    关机

---
+++ Close.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Close
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CloseWindow();
+        }
+
+        public void CloseWindow()
+        {
+            System.Diagnostics.Process bootProcess = new System.Diagnostics.Process();
+            bootProcess.StartInfo.FileName = "shutdown";
+            bootProcess.StartInfo.Arguments = "/s";
+            bootProcess.Start();
+        }
+
+
+    }
+}
